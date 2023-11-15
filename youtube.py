f7056f4b0837b089f52745a01d2ac06cc7aaadac
Gabriel de Oliveira Souza <64101105+gabrielsouza-oss@users.noreply.github.com>
2023-11-15 11:34:04 -0300

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -150,5 +150,5 @@ if __name__ == '__main__':
 
   try:
     initialize_upload(youtube, args)
-  except HttpError, e:
+  except (HttpError, e):
     print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
