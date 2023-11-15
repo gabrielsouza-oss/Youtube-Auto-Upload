68037556a3a70a6a4c84dacc0504b2b351965219
Gabriel de Oliveira Souza <64101105+gabrielsouza-oss@users.noreply.github.com>
2023-11-15 12:00:41 -0300

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -56,7 +56,7 @@ VALID_PRIVACY_STATUSES = ('public', 'private', 'unlisted')
 # Authorize the request and store authorization credentials.
 def get_authenticated_service():
   flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-  credentials = flow.run_local_server(port=0)
+  credentials = flow.run_console()
   return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
 
 def initialize_upload(youtube, options):
